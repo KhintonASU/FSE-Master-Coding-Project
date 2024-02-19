336c12cbc52943a1892a7076416775e5a5ccd9cb
KhintonASU <160547382+KhintonASU@users.noreply.github.com>
2024-02-19 16:17:55 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,14 +4,7 @@ function setup() {
 
 function draw() {
   background(127);
-  noStroke();
-  for (let i = 0; i < height; i += 20) {
-    fill(129, 206, 15);
-    rect(0, i, width, 10);
-    fill(255);
-    rect(i, 0, 10, height);
-    fill("#000");
-    stroke("#80FF00");
     text("Noah",150,100);
-  }
+    text("kavious",140,90):
+  
 }
